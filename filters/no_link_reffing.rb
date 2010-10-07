5d59a7b3593c973505d3adfdc16efa94f3e0537a
Iain Barnett <iainspeed@gmail.com>
2010-10-07 17:07:43 +0100

    added embedding links as video too, and no link reffing for summaries

---
+++ filters/no_link_reffing.rb
@@ -0,0 +1,19 @@
+class No_link_reffing < Nanoc3::Filter
+  identifier :no_link_reffing
+ 
+  def run(content, params={})
+    
+    r = /             # [[link|description]]
+          \[(?:embed)?\[        # opening square brackets
+            \S+     # link
+              \|      # separator
+            ([^\[]+)  # description
+          \]\]        # closing square brackets
+        /x 
+    
+    replacement = content.gsub( r, '' )
+    
+  end
+
+
+end
